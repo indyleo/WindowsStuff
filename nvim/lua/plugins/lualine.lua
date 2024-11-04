24e0dc170858b32008bfa202ae6579e3c85ebb2f
Indy <100316787+indyleo@users.noreply.github.com>
2024-11-03 19:53:35 -0500

    Update lualine.lua

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,6 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
     -- configure lualine with modified theme
     lualine.setup({
       options = {
@@ -11,11 +10,6 @@ return {
       },
       sections = {
         lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
           { "encoding" },
           { "fileformat" },
           { "filetype" },
