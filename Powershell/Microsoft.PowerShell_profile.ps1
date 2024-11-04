74b7a53752c811a293e46e3890d286cb7058e99f
Indy <100316787+indyleo@users.noreply.github.com>
2024-11-04 13:43:04 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -29,7 +29,7 @@ if (-not (Test-IsRunningInNeovim)) {
 }
 
 # Varables Configs
-$VISUAL = if (Test-CommandExists codium) { 'codium' } else { Write-Host "VSCodium is not installed." }
+$VISUAL = if (Test-CommandExists neovide) { 'neovide' } else { Write-Host "Neovide is not installed." }
 $EDITOR = if (Test-CommandExists nvim) { 'nvim' } else { Write-Host "Neovim ias not installed." }
 $AlIAS = "$HOME\Documents\Powershell\Sources\Aliases.ps1"
 $FUNCTION = "$HOME\Documents\Powershell\Sources\Functions.ps1"
