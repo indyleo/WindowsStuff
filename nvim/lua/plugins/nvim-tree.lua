1210965ac4d08d9aae1fb3ea4c79d5236bb7daa4
Indy <100316787+indyleo@users.noreply.github.com>
2024-11-04 10:41:30 -0500

    Update nvim-tree.lua

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -3,11 +3,6 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     local nvimtree = require("nvim-tree")
-
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
     nvimtree.setup({
       view = {
         width = 35,
