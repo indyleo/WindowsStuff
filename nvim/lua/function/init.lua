cf16338725c5296c9a7f5c3b4b7878b45e0d72b3
Indy <100316787+indyleo@users.noreply.github.com>
2024-11-07 19:03:48 -0500

    Delete nvim directory

--- nvim/lua/function/init.lua
+++
@@ -1,5 +0,0 @@
--- Files
-require "function.SpellCheck"
-require "function.SourceConf"
-require "function.NumOpts"
-require "function.HttpServer"
--- nvim/lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "ap/vim-css-color",
-  "famiu/bufdelete.nvim",
-  "karb94/neoscroll.nvim",
-  config = function ()
-    require('neoscroll').setup({})
-  end
-}
