41710f29267145c0468fac3907eea893a7517032
Indy <100316787+indyleo@users.noreply.github.com>
2024-11-19 19:29:14 -0500

    Update alpha.lua

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -16,7 +16,6 @@ return {
       "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
       "                                                     ",
     }
-
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", ":lua AskNewFileName()<CR>"),
@@ -24,9 +23,11 @@ return {
       dashboard.button("SPC ff", "󰱼  > Find File", ":Telescope find_files<CR>"),
       dashboard.button("ce", "  > Config Nvim", ":e " .. vim.fn.stdpath("config") .. "<CR>"),
       dashboard.button("ct", "  > Config WezTerm", ":e " .. vim.loop.os_homedir() .. "/.config/wezterm<CR>"),
+      dashboard.button("ct", "  > Config FastFetch", ":e " .. vim.loop.os_homedir() .. "/.config/fastfetch<CR>"),
+      dashboard.button("cp", "  > Config Powershell", ":e " .. vim.loop.os_homedir() .. "/Documents/Powershell<CR>"),
       dashboard.button("q", "  > Quit Nvim", ":qa<CR>"),
     }
-
+  
     -- Send config to alpha
     alpha.setup(dashboard.opts)
 
