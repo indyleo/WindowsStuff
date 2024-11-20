b6fb0bdf848b07c68859b9db9cbeab69695037f7
Indy <100316787+indyleo@users.noreply.github.com>
2024-11-20 13:17:48 -0500

    Delete nvim directory

--- nvim/lua/plugins/nvim-ufo.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "kevinhwang91/nvim-ufo",
-  dependencies = "kevinhwang91/promise-async",
-  name = "ufo",
-  config = function ()
-    local ufo = require("ufo")
-    ufo.setup()
-  end
-}
